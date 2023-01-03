8dcaa2ccfa825385b6d432bcf3b71ab9f96185f8
Kyle <kmthayer@uw.edu>
2023-01-03 11:19:25 -0800

    start simple nodejs demo

---
+++ lecture_01_simple_servers/Demo_1_nodejs/app.js
@@ -0,0 +1,22 @@
+console.log("hello world")
+
+let years = 1
+let days = years * 365
+let hours = days * 24
+let minutes = hours * 60
+
+console.log("There are " + minutes + " minutes in a year")
+
+// note these three ways of writing functions are equivalent
+
+function example_function(a, b) {
+  return a + b;
+}
+
+let example_function = function(a, b) {
+    return a + b;
+}
+
+let example_function = (a, b) => {
+    return a + b
+}
