3017493dabea97acb9a96d9881c2dc754fc0bd97
Kyle <kmthayer@uw.edu>
2023-01-05 11:12:06 -0800

    added async demo

---
+++ lecture_02_async_ajax_filesystem/demo_1_async/app.js
@@ -0,0 +1,22 @@
+
+function resolveAfterNSeconds(n){
+    return new Promise(resolve => {
+        setTimeout(() => {resolve("finished")}, 1000*n)
+    })
+}
+
+async function testAwait(){
+    console.log("about to wait")
+    await resolveAfterNSeconds(5)
+    console.log("finished the 5 second wait")
+}
+
+testAwait()
+
+async function testAwait2(){
+    console.log("Test 2 about to wait")
+    await resolveAfterNSeconds(2)
+    console.log("test 2 finished the 2 second wait")
+}
+
+testAwait2()
