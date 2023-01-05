ccffdcea6d21cd6b717f927f1e580ea30e16fe80
Kyle <kmthayer@uw.edu>
2023-01-05 12:22:24 -0800

    added the two ajax demos

---
+++ lecture_02_async_ajax_filesystem/demo_3_current_time/index.js
@@ -0,0 +1,7 @@
+
+async function checkTime(){
+    let response = await fetch("api/getTime")
+    let resultText = await response.text()
+
+    document.getElementById("results").innerHTML = resultText
+}
