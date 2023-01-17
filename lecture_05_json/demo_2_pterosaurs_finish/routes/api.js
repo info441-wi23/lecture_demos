5e9fcc311ca2592e19bfbee7e042ea36d8abdfa1
Kyle <kmthayer@uw.edu>
2023-01-17 11:32:27 -0800

    finish pterosaur demo

--- lecture_05_json/demo_2_pterosaurs_finish/routes/api.js
+++ lecture_05_json/demo_2_pterosaurs_finish/routes/api.js
@@ -6,9 +6,20 @@ router.get("/pterosaurs", async (req, res) => {
     // load the data file
     const data = await fs.readFile("data/pterosaur.json")
 
+    // convert data to json object
+    let pterosaurJson = JSON.parse(data)
+
+    // filter for those that have images
+    let filterdPterosaurJson = pterosaurJson.filter(pterosaurEntry => {
+        if(pterosaurEntry.img != ""){
+            return true
+        }{
+            return false
+        }
+    })
+
     // send the contents back
-    res.type("json")
-    res.send(data)
+    res.json(filterdPterosaurJson)
 })
 
 export default router
