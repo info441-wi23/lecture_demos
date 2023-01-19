ee3799f4c6f9606d78891332239eb094137e4799
Kyle <kmthayer@uw.edu>
2023-01-19 12:07:28 -0800

    add error handling

--- lecture_06_mongodb_middleware/demo_2_user_upload_finish/routes/controllers/users.js
+++ lecture_06_mongodb_middleware/demo_2_user_upload_finish/routes/controllers/users.js
@@ -5,23 +5,33 @@ var router = express.Router();
 
 router.post('/', async (req, res) => {
   console.log(req.body)
+  try{
+    const newUser = new req.models.User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      favorite_ice_cream: req.body.favorite_ice_cream,
+      another_piece_of_info: "ha ha"
+    })
 
-  const newUser = new req.models.User({
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    favorite_ice_cream: req.body.favorite_ice_cream
-  })
+    await newUser.save()
 
-  await newUser.save()
-
-  res.send("success")
+    res.send("success")
+  }catch(error){
+    console.log("Error saving user: ", error)
+    res.status(500).json({"status": "error", "error": error})
+  }
 })
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  let allUsers = await req.models.User.find()
-  console.log(allUsers)
-  res.json(allUsers)
+  try{
+    let allUsers = await req.models.User.find()
+    console.log(allUsers)
+    res.json(allUsers)
+  }catch(error){
+    console.log("Error saving user: ", error)
+    res.status(500).json({"status": "error", "error": error})
+  }
 });
 
 export default router;
