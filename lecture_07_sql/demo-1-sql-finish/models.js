0eb3360e631316a68fbd9625df761d2534bc3627
Kyle <kmthayer@uw.edu>
2023-01-24 10:59:29 -0800

    finish sql demo

--- lecture_07_sql/demo-1-sql-finish/models.js
+++ lecture_07_sql/demo-1-sql-finish/models.js
@@ -1,3 +1,33 @@
+import sqlite3 from 'sqlite3'
+
 let models = {}
+
+models.db = new sqlite3.Database(':memory:', (err) => {
+    if(err)
+    {
+        return console.error(err.message)
+    }
+    console.log("Connected to the in-memory sqlite database")
+})
+
+
+models.db.serialize(() => {
+    models.db
+        .run('CREATE TABLE people(first_name text, last_name text)')
+        .run(`INSERT INTO people(first_name, last_name)
+              VALUES ("Kyle", "Thayer"),
+                     ("Kyle", "Chandler"),
+                     ("Miranda", "Ma"),
+                     ("Jared", "Lim")
+        
+        `)
+    
+        .run('CREATE TABLE secret_table(message text)')
+        .run(`INSERT INTO secret_table(message)
+              VALUES ('The password for Kyle is: pa55w0rd'),
+                     ('The treasure is hidden on the 5th floor.')
+         
+        `)
+})
 
 export default models
