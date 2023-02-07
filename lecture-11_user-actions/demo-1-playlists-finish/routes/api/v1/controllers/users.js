bc86d4dab86593bc26d158ede7bd1ad2e399cb0a
Kyle <kmthayer@uw.edu>
2023-02-07 11:07:31 -0800

    make save user function work

---
+++ lecture-11_user-actions/demo-1-playlists-finish/routes/api/v1/controllers/users.js
@@ -0,0 +1,22 @@
+import express from 'express'
+let router = express.Router()
+
+router.post("/", async(req, res) => {
+    try{
+        let username = req.body.username
+        
+        let newUser = new req.models.User({
+            username: username
+        })
+
+        await newUser.save()
+
+        res.json({status: 'success'})
+    } catch(err){
+        console.log("error: ", err)
+        res.status(500).json({status: "error"})
+    }
+
+})
+
+export default router
