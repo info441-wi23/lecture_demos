2f50d21aa48112f78eccc7b875e3193cc79c8de3
Kyle <kmthayer@uw.edu>
2023-02-09 11:10:29 -0800

    do most of socket demo

--- lecture-12_websockets/demo-2-websockets-finish/app.js
+++ lecture-12_websockets/demo-2-websockets-finish/app.js
@@ -1,6 +1,29 @@
 import express from 'express'
+import enableWs from 'express-ws'
 
 const app = express()
+enableWs(app)
+
+let socketCounter = 1
+let allSockets = []
+
+app.ws('/chatSocket', (ws, res) => {
+    let mySocketNum = socketCounter
+    socketCounter++;
+    allSockets.push(ws)
+
+    console.log("user " + mySocketNum + " connected")
+
+    ws.on('message', msg => {
+        console.log("msg (user " + mySocketNum + "): " + msg)
+        // TODO: Send the message across all the websockets
+        // to all the other browsers
+        allSockets.forEach( socket => {
+            socket.send(mySocketNum + ": " + msg)
+        })
+    })
+})
+
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + "/index.html")
