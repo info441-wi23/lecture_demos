5eefd8b5a7e3178f8d041cdae0432527344ec156
Kyle <kmthayer@uw.edu>
2023-02-23 11:21:23 -0800

    set up react for reverse proxy

--- lecture-16_microservices/microservices-finish/express-reverse-proxy/app.js
+++ lecture-16_microservices/microservices-finish/express-reverse-proxy/app.js
@@ -2,14 +2,10 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import httpProxyMiddleware from 'http-proxy-middleware'
+const createProxyMiddleware = httpProxyMiddleware.createProxyMiddleware
 
 import usersRouter from './routes/users.js';
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 var app = express();
 
@@ -17,7 +13,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 
@@ -32,5 +27,7 @@ app.get('/api/square', (req, res) => {
     let squared = num * num
     res.send("" + squared)
 })
+
+app.use('/*', createProxyMiddleware({target: 'http://localhost:4000'}))
 
 export default app;
