e78423e22a384489dd1f83f5ca150ba741399188
Kyle <kmthayer@uw.edu>
2023-02-23 11:39:23 -0800

    add square microservice (which we will run on two servers)

--- lecture-16_microservices/microservices-finish/express-reverse-proxy/app.js
+++ lecture-16_microservices/microservices-finish/express-reverse-proxy/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import request from 'request';
 import httpProxyMiddleware from 'http-proxy-middleware'
 const createProxyMiddleware = httpProxyMiddleware.createProxyMiddleware
 
@@ -18,11 +19,22 @@ app.use('/users', usersRouter);
 
 app.get('/api/double', createProxyMiddleware({target: 'http://localhost:5000'}))
 
-app.get('/api/square', (req, res) => {
-    let num = req.query.num
-    let squared = num * num
-    res.send("" + squared)
+
+
+const servers = ['http://localhost:6001', 'http://localhost:6002' ];
+let cur_server_index = 0;
+app.use('/api/square', (req, res) => {
+  try {
+  	cur_server_index = (cur_server_index  + 1) % servers.length;
+  	req.pipe(request({ url: servers[cur_server_index] + req.originalUrl })).pipe(res);
+  } catch (error) {
+	console.log("error in /api/square:" + error)
+	res.status(500).json({status: "error", error: error});
+  }
 })
+
+
+
 
 app.use('/*', createProxyMiddleware({target: 'http://localhost:4000'}))
 
