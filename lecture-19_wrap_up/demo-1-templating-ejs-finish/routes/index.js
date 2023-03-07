5e2f7cd7dbaa5b0949dc64cc2f5f60302a6e2a38
Kyle <kmthayer@uw.edu>
2023-03-07 11:25:14 -0800

    finish templating engine demo

--- lecture-19_wrap_up/demo-1-templating-ejs-finish/routes/index.js
+++ lecture-19_wrap_up/demo-1-templating-ejs-finish/routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // TODO: We could look up azure session info here
+  let username = "<strong>Kyle</strong>"
+
+  // TODO: we could look up info from our database
+  let produce = [
+    {name: "apple", price: 1.5},
+    {name: "orange", price: .75},
+    {name: "banana", price: 10}
+  ]
+
+  res.render('index', { 
+    title: 'Produce Store',
+    username: username,
+    produce: produce
+  });
 });
 
 module.exports = router;
